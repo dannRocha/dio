474371dbd0eca881e7b376e8e56e474b68b1059e
Daniel Rocha <rochadaniel15@gmail.com>
2021-05-06 23:08:48 -0300

    add: [Azure Spring Cloud]  Adicionado pacote de controllers com o controller de Status e User

---
+++ projeto-azure-cloud/src/main/java/projeto/azure/jwt/controller/StatusController.java
@@ -0,0 +1,28 @@
+package projeto.azure.jwt.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import projeto.azure.jwt.service.UserDetailsServiceImpl;
+
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+
+
+  @GetMapping
+  public String status() {
+    return "online";
+  }
+}
