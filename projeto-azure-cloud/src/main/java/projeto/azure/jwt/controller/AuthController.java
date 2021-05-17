4e544eea74bc6d382ac2617314ff0f66c492c86d
Daniel Rocha <rochadaniel15@gmail.com>
2021-05-17 15:53:01 -0300

    add: [Azure Spring Cloud]  Adicionado implementação do login com JTW

---
+++ projeto-azure-cloud/src/main/java/projeto/azure/jwt/controller/AuthController.java
@@ -0,0 +1,23 @@
+package projeto.azure.jwt.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import projeto.azure.jwt.data.UserData;
+
+@RestController
+public class AuthController {
+  
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  public AuthController(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+  }
+
+  @PostMapping("/login")
+  public void login(@RequestBody UserData user) {
+    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+  }
+}
--- projeto-azure-cloud/src/main/java/projeto/azure/jwt/sercurity/WebSecurity.java
+++ projeto-azure-cloud/src/main/java/projeto/azure/jwt/sercurity/WebSecurity.java
@@ -35,11 +35,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
       .permitAll()
       .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
       .permitAll()
-      .anyRequest().authenticated();
-      //.and()
-      //.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+      .anyRequest().authenticated()
+      .and()
+      .addFilter(new JWTAuthenticationFilter(authenticationManager()))
       //.addFilter(new JWTAuthorizationFilter(authenticationManager()))
-      //.sessionManagement().SessionCreationPolicy(SessionCreationPolicy.STATELESS);
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   
   }
 
